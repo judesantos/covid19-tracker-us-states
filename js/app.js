83c951c7d7dee232893d78566d2fe840b8ddf594
debimsantos <debimortola@gmail.com>
2020-04-01 10:28:15 -0700

    news formatting and file restructuring

--- js/app.js
+++ js/app.js
@@ -54,38 +54,6 @@ formatDate = (date) => {
     return new Intl.DateTimeFormat(locale, options).format(new Date(date));
 }
 
-// ------------------------------------------
-// NEWS
-// ------------------------------------------
-
-let getCovid19News = async () => {
-    let dataItems = {};
-    response = await fetch('https://covidtracking.com/api/press');
-    const jsonData = await response.json();
-    // process data
-    if (jsonData.length) {
-        let newsItems = '';
-        for (item of jsonData) {
-            newsItems +=
-                '<div class="news-item">' +
-                '<a class="news-details" target="_blank"' +
-                'href="' + item.url + '">' + item.title + '</a>' +
-                '<div class="news-source">' +
-                item.publication + ', ' + item.publishDate +
-                '</div>' +
-                '</div>';
-        }
-        return newsItems;
-    }
-    return '';
-};
-
-getCovid19News().then(newsItems => {
-    let elNewsItems = document.getElementById('news_items');
-    if (elNewsItems) {
-        elNewsItems.innerHTML = newsItems;
-    }
-});
 
 // ------------------------------------------
 // MAP CHART
@@ -232,7 +200,7 @@ getCurrentDataPerState().then(covidData => {
                     style: {
                         hoverState: {
                             visible: false // highlight state on hover
-                        }, 
+                        },
                         controls: {
                             placement: 'tr'
                         },
